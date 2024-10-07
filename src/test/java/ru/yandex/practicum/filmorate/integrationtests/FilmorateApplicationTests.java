4a07bd47510d6706f902d23cc09da37441428a60
aKorishev <korishev.a@yandex.ru>
2024-10-07 14:21:59 +0300

    Добавил реализацию интеграционного теста

--- src/test/java/ru/yandex/practicum/filmorate/integrationtests/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/integrationtests/FilmorateApplicationTests.java
@@ -1,29 +1,61 @@
 package ru.yandex.practicum.filmorate.integrationtests;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
 import ru.yandex.practicum.filmorate.storage.dbstorage.InDbUserStorage;
 import ru.yandex.practicum.filmorate.storage.dbstorage.mappers.UserRowMapper;
+import ru.yandex.practicum.filmorate.storage.dbstorage.repositories.FriendsRepository;
 import ru.yandex.practicum.filmorate.storage.dbstorage.repositories.UsersRepository;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @JdbcTest
 @AutoConfigureTestDatabase
-@RequiredArgsConstructor(onConstructor_ = @Autowired)
+//@RequiredArgsConstructor(onConstructor_ = @Autowired)
 @ComponentScan("ru.yandex.practicum.filmorate")
 @Import({UsersRepository.class, UserRowMapper.class})
-//Как обычно, класс положили, а чтобы он работал не положили
 public class FilmorateApplicationTests {
-    private final InDbUserStorage userStorage;
+    private static UserStorage userStorage;
 
-//    @Test
-//    public void testFindUserById() {
-//
-//        User user = userStorage.getUser(1);
-//
-//        assertThat(user).hasFieldOrPropertyWithValue("id", 1);
-//    }
+    @BeforeAll
+    public static void init() {
+        var config = new HikariConfig();
+        config.setJdbcUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;INIT=runscript from 'classpath:/schema.sql'");
+        config.setUsername("database_username");
+        config.setPassword("database_password");
+
+        var dataSource = new HikariDataSource(config);
+        var jdbcTemplate = new JdbcTemplate(dataSource);
+
+        var userRowMapper = new UserRowMapper();
+
+        userStorage = new InDbUserStorage(
+                new UsersRepository(jdbcTemplate, userRowMapper),
+                new FriendsRepository(jdbcTemplate, userRowMapper));
+    }
+
+    @Test
+    public void testFindUserById() {
+        userStorage.createUser(
+                User.builder()
+                        .email("dfgd@email.ru")
+                        .login("login")
+                        .birthday(LocalDate.now())
+                        .build());
+
+        User user = userStorage.getUser(1);
+
+        assertThat(user).hasFieldOrPropertyWithValue("id", 1L);
+    }
 }
